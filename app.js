7f662abcaf970e82458aaa24acc63193fd5bebd8
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-10 13:56:36 +0545

    update post route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require("express-handlebars");
 // load routes
 const home = require("./routes/home/home");
 const admin = require('./routes/admin/admin');
+const posts = require('./routes/admin/posts');
 
 // set static folder
 app.use(express.static(__dirname + "/public"));
@@ -16,6 +17,7 @@ app.set('view engine', 'handlebars');
 // use routes
 app.use("/",home);
 app.use("/admin",admin);
+app.use("/admin/posts",posts);
 
 
 const port = process.env.PORT || 5000;
